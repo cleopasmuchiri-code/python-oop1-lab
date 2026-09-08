03e7efe670a3079f8007ec7a1d1629672a3809f4
CleopasMMuchiri <cleopasmmuchiri@gmail.com>
2026-09-08 13:16:38 +0300

    assignment pushed

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+        if self.size not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        self.price += 1
+        print("This coffee is great, here’s a tip!")
